d9aad7a23abdeea755e5921fb3a35e623aa4438b
Max Field <maxfield7766@gmail.com>
2023-03-01 21:47:14 +1300

    Work session 1

---
+++ Unit-4-game0/Assets/Scripts/MoveCamera.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveCamera : MonoBehaviour
+{
+    public GameObject player;
+    // Start is called before the first frame update
+    void Start()
+    {
+        player = GameObject.FindWithTag("Player");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.position = new Vector3(player.transform.position.x / 5, 10, -20);
+
+        
+    }
+}
---
+++ Unit-4-game0/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public Rigidbody rb;
+    public float speed;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+
+        transform.Translate(Vector3.forward * Time.deltaTime * vertical * speed);
+        transform.Translate(Vector3.right * Time.deltaTime * horizontal * speed);
+    }
+}
